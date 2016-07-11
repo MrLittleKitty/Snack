f9a1c5583eb946293da9fa8078647f50024d9f4d
Mr_Little_Kitty <nuclearcat1337@gmail.com>
2016-07-10 23:04:48 -0700

    Added some comments and create default settings file

--- src/main/java/com/gmail/nuclearcat1337/snack/Snack.java
+++ src/main/java/com/gmail/nuclearcat1337/snack/Snack.java
@@ -10,10 +10,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
@@ -28,36 +25,49 @@ public class Snack
     public static final String MODID = "snack";
     public static final String MODNAME = "Snack";
     public static final String MODVERSION = "1.0.0";
-
-    private static final Pattern snitchAlertPattern =
-            Pattern.compile("\\s*\\*\\s*([^\\s]*)\\s\\b(?:entered snitch at|logged out in snitch at|logged in to snitch at)\\b\\s*([^\\s]*)\\s\\[([^\\s]*)\\s([-\\d]*)\\s([-\\d]*)\\s([-\\d]*)\\]");
     private static final Logger logger = Logger.getLogger(MODID);
 
+    //A regular expression that matches all 3 of the snitch alert messages (log in, log out, enter)
+    private static final Pattern snitchAlertPattern =
+            Pattern.compile("\\s*\\*\\s*([^\\s]*)\\s\\b(?:entered snitch at|logged out in snitch at|logged in to snitch at)\\b\\s*([^\\s]*)\\s\\[([^\\s]*)\\s([-\\d]*)\\s([-\\d]*)\\s([-\\d]*)\\]");
+
+    //The slack session to post messages to
     private SlackSession session = null;
 
+    //The token used to connect to the slack session
     private String token = null;
+
+    //The slack channel to post messages to
     private String channel = null;
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
+        //Load the channel and token from the settings file
         boolean success = loadSettings(new File(Minecraft.getMinecraft().mcDataDir,"mods/Snack/settings.txt"));
 
+        //If we loaded the token and channel, continue
         if(!success || token == null || channel == null)
         {
             logger.info("[Snack] SHUTTING DOWN because could not load token or channel");
+            //By returning at this point we dont register any listeners and are effectively shut down
             return;
         }
 
-        session = SlackSessionFactory.createWebSocketSlackSession(token);
         try
         {
+            //Create the slack session
+            session = SlackSessionFactory.createWebSocketSlackSession(token);
+
+            //Open the connection
             session.connect();
         }
         catch (IOException e)
         {
             e.printStackTrace();
             logger.info("[Snack] SHUTTING DOWN because could not create slack session");
+
+            //By returning at this point we stop any events from being registed and are effectively shut down
             return;
         }
 
@@ -71,37 +81,64 @@ public class Snack
     {
         if(event != null && event.getMessage() != null)
         {
+            //Get the message we got in chat
             String message = event.getMessage().getUnformattedText();
+
+            //Get the Matcher object for the regular expression that matches snitch alerts
             Matcher matcher = snitchAlertPattern.matcher(message);
+
+            //If the message matches a snitch alert, send it in slack
             if(matcher.matches())
             {
-                SlackChannel slackChannel = session.findChannelByName(channel);
+                try
+                {
+                    //Get the channel that we are supposed to send messages to
+                    SlackChannel slackChannel = session.findChannelByName(channel);
 
-                session.sendMessage(slackChannel,message);
+                    //Send the alert message to that channel
+                    session.sendMessage(slackChannel,message);
+                }
+                catch(Exception e)
+                {
+                    logger.info("[Snack] WARNING: Could not post alert message to Slack.");
+
+                    //If we cant post messages anymore, unregister this and effectively shut down
+                    MinecraftForge.EVENT_BUS.unregister(this);
+                }
             }
         }
     }
 
     private boolean loadSettings(File file)
     {
+        //See if the settings file exists
         if(file.exists())
         {
             try
             {
+                //Attempt to load the values from the settings file
                 BufferedReader reader = new BufferedReader(new FileReader(file));
                 String line = null;
                 while((line = reader.readLine()) != null)
                 {
                     String[] tokens = line.split("=");
+                    //See if this is the line with the slack token
                     if(tokens[0].equalsIgnoreCase("token"))
                     {
                         token = tokens[1];
+                        //If this is the default value, dont use it
+                        if(token.equalsIgnoreCase("placeholder"))
+                            token = null;
                     }
-                    else if(tokens[0].equalsIgnoreCase("channel"))
+                    else if(tokens[0].equalsIgnoreCase("channel")) //See if this is the line with the slack channel name
                     {
                         channel = tokens[1];
+                        //If this is the default value, dont use it
+                        if(channel.equalsIgnoreCase("placeholder"))
+                            channel = null;
                     }
                 }
+                //Always close your asset streams, kids.
                 reader.close();
                 return true;
             }
@@ -111,6 +148,23 @@ public class Snack
                 return false;
             }
         }
-        else return false;
+        else
+        {
+            //If the settings file doesnt exist, create it and set the default values
+            try
+            {
+                file.createNewFile();
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                writer.write("token=PLACEHOLDER");
+                writer.write(System.lineSeparator());
+                writer.write("channel=PLACEHOLDER");
+                writer.flush();
+                writer.close();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            } return false;
+        }
     }
 }
